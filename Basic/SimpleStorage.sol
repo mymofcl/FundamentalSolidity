83e824327090390151f307f1c9aed52cea3966b3
mymofcl <mymofcl@gmail.com>
2024-12-15 23:24:17 +0600

    Updating at 15

--- Basic/SimpleStorage.sol
+++ Basic/SimpleStorage.sol
@@ -2,10 +2,12 @@
 pragma solidity ^0.8.0;
 
 contract SimpleStorage{
+    bool public favNumber;
     uint public storedData;
 
-    function store(uint x) public {
+    function store(uint x, bool y) public {
         storedData = x;
+        favNumber = y;
     }
     function retrieve() public view returns (uint) {
        return storedData; 
