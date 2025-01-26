001b66f1006f043b09af2e21413d136959d76aa6
mymofcl <mymofcl@gmail.com>
2025-01-26 09:21:26 +0600

    Basic Datatypes

---
+++ Basic/intro.sol
@@ -0,0 +1,22 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract DataType{
+    uint public favNum;
+    bool public hasFavNum;
+
+    function setFavNumber(uint _num)public {
+        favNum = _num;
+        hasFavNum = true;
+    }
+    function getFavNum() public view returns (uint){
+        return favNum;
+    }
+    function toggle() public payable {
+        if(hasFavNum){
+            hasFavNum = !hasFavNum;
+            delete favNum;
+        }
+    }
+
+}
